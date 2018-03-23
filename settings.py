6cfbda6a216aa5e4036da961bab13f8a304937fe
30ankitbansal <30ankitbansal@gmail.com>
2018-03-24 03:42:16 +0530

    final

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ Bitstamp_client_id = 'gugo5564'                                # customer ID
 Ice_key = 'IYBV1EGYESOGEHACTSKQ0ERQIMEJ0DQBGEDPCTTSARDLWNRIEFVVFLOO5TDGAXHY'                    # public key
 Ice_secret = 'cyv4pnqqxvqhdp6minydzjpsdgcnlxuieckgi8pilkfnuxgsbuk89lbwq6s1alpe'                 # private key
 
-EMAIL_FROM = '30ankitbansal@gmail.com'                 # Email_id from which you want send email only GOOGLE account
-EMAIL_PASSWORD = '@nkitone97'             # GOOGLE account password
-EMAIL_TO = ['30ankitbansal@gmail.com']                   # Email_id on which you want receive email
-EMAIL_CC = ['abhisheksoni500@gmail.com']                   # list of email_ids
+EMAIL_FROM = ''                 # Email_id from which you want send email only GOOGLE account
+EMAIL_PASSWORD = ''             # GOOGLE account password
+EMAIL_TO = ['']                   # Email_id on which you want receive email
+EMAIL_CC = ['']                   # list of email_ids
 
